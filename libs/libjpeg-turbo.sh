e6525d19976751c4af4ea0c69df7fc49e9bbd0f4
Wesley Chan <qlb1234@gmail.com>
2014-03-18 17:26:09 +0800

    Build shared libraries only.

--- libs/libjpeg-turbo.sh
+++ libs/libjpeg-turbo.sh
@@ -4,7 +4,7 @@ cd libjpeg-turbo
 tar -xf libjpeg-turbo-1.3.0.tar.gz
 cd libjpeg-turbo-1.3.0
 
-./configure --prefix=$PREFIX 2>&1 | tee ../../logs/libjpeg-turbo-configure.log
+./configure --disable-static --prefix=$PREFIX 2>&1 | tee ../../logs/libjpeg-turbo-configure.log
 make -j $NUMBER_OF_PROCESSORS 2>&1 | tee ../../logs/libjpeg-turbo-make.log
 make install 2>&1 | tee ../../logs/libjpeg-turbo-makeinstall.log
 
